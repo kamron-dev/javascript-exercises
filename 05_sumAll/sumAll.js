bb9ee345bbf12582855ab7e6154551aa471c6ea5
kamron-dev <109903041+kamron-dev@users.noreply.github.com>
2022-12-27 16:15:38 +0500

    done these

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let finNum = 0;
+    if (startNum < 0 || endNum < 0) return "ERROR"
+    if (typeof startNum !== "number" || typeof endNum !== "number") return "ERROR"
+    if (startNum > endNum) {
+        let temporary = endNum;
+        endNum = startNum;
+        startNum = temporary;
 
+    }
+    for (let i = startNum; i <= endNum; i++) {
+        finNum += i
+    }
+    
+    return finNum
 };
 
 // Do not edit below this line
